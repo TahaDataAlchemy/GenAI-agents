e7ce89a211f7b095f4329f89a1b3aef0d4009976
Taha Mehboob <157484236+TahaDataAlchemy@users.noreply.github.com>
2025-04-15 14:20:46 -0700

    Add files via upload

--- DeepResearcher/data/dataModeling.py
+++ DeepResearcher/data/dataModeling.py
@@ -1,12 +1,14 @@
 from pydantic import BaseModel, Field
 from typing import Optional, TypedDict
 
-class TextInput(BaseModel):
+class SearchInput(BaseModel):
     """
     Model for the user input. Either a search query or a direct link must be provided.
     """
     query: str = Field(..., description="Search query provided by the user")
-    link: Optional[str] = Field(None, description="Optional direct link to fetch data from")
+    site: Optional[str] = Field(None, description="Optional direct link to fetch data from")
+    max_results: Optional[int] = Field(1, gt=0, le=50, description="Number of results to fetch (1-50)")
+
 
 class UserResponseApi(TypedDict):
     """
