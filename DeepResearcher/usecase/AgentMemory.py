bced86e80456f74cb223e24876c4e08a67b0493b
Taha Mehboob <157484236+TahaDataAlchemy@users.noreply.github.com>
2025-04-24 13:56:32 -0700

    Add files via upload

--- DeepResearcher/usecase/AgentMemory.py
+++ DeepResearcher/usecase/AgentMemory.py
@@ -1,3 +1,37 @@
-import json
+from pymongo import MongoClient
 from datetime import datetime
-from typing import List ,Dict
+from app.config import settings
+class AgentMemory:
+    def __init__(self,db_uri = settings.MONGO_URI,db_name = settings.DB_NAME,collection_name = settings.UserMemory):
+        self.client = MongoClient(db_uri)
+        self.collection = self.client[db_name][collection_name]
+    
+    def add(self,query,summary,related_questions,relevent_link):
+        self.collection.insert_one(
+            {
+                "query":query,
+                "summary":summary,
+                "related_question":related_questions,
+                "relevent_link":relevent_link,
+                "timestamp":datetime.utcnow()
+            }
+        )
+    def get_all(self):
+        return list(self.collection.find({},{"_id":0})) # empty dictionary means all values and _id:0 means i dont need id field
+
+    def get_last(self):
+        return self.collection.find_one(sort = ["timestamp",-1],projection = {"_id":0}) # projection doing the same as it is excluding the _id
+
+    def search_context(self,keyword): # it avoid search and based on previous queries fetch the similar ones
+        return list(
+            self.collection.find(
+                {"query":{"$regex":keyword,"$option":"i"}},
+                {"_id":0}
+            )
+        )
+    def clear_memory(self):
+        self.collection.delete_many({})
+    
+    def suggest_next_queries(self,recent_count=3):
+        recent = self.collection.find({},{"_id":0,"query":1}).sort("timestamp",-1).limit(recent_count)
+        return [q["query"] for q in recent]
